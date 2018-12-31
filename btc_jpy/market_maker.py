afd4abb264d5321e5a91af4745320206862c36d8
Hokuto Tokutake <tokutake.hokuto@gmail.com>
2018-12-31 13:43:58 +0900

    Use board to obtain best ask and best bid

--- btc_jpy/market_maker.py
+++ btc_jpy/market_maker.py
@@ -14,7 +14,7 @@ bitflyer.load_markets()
 
 open_orders = {}
 
-lot = 0.01
+lot = 0.001
 
 def create_buy_order(price):
     o = bitflyer.create_limit_buy_order(symbol, lot, price)
@@ -32,17 +32,62 @@ def create_sell_order(price):
     o['side'] = 'sell'
     open_orders[o['id']] = o
 
+bids = {}
+asks = {}
+mid_price = None
+
+def update_board(message_json):
+    mid_price = message_json['mid_price']
+    for bid in message_json['bids']:
+        bids[bid['price']] = bid['size']
+    for ask in message_json['asks']:
+        asks[ask['price']] = ask['size']
+
+def best_ask():
+    ask = None
+    for price, size in asks.items():
+        if size == 0:
+            continue
+        if ask == None:
+            ask = price
+        if price < ask:
+            ask = price
+    return ask
+
+def best_bid():
+    bid = None
+    for price, size in bids.items():
+        if size == 0:
+            continue
+        if bid == None:
+            bid = price
+        if price > bid:
+            bid = price
+    return bid
+
+def print_board():
+    for price, size in asks.items():
+        print('price {}, size {}'.format(price, size))
+    for price, size in bids.items():
+        print('price {}, size {}'.format(price, size))
+
+
 def on_message(ws, message):
-    message_json = json.loads(message)
-    param_json = message_json['params']
+    param_json = json.loads(message)['params']
     channel = param_json['channel']
-    if channel == 'lightning_ticker_' + channel_symbol:
-        info = param_json['message']
-        best_bid = info['best_bid']
-        best_ask = info['best_ask']
-        if len(open_orders) == 0:
-            create_buy_order(best_bid + 1)
-            create_sell_order(best_ask - 1)
+    message_json = param_json['message']
+    if channel == 'lightning_board_snapshot_' + channel_symbol:
+        bids = {}
+        asks = {}
+        update_board(message_json)
+
+    if channel == 'lightning_board_' + channel_symbol:
+        update_board(message_json)
+        ask = best_ask()
+        bid = best_bid()
+        if len(open_orders) == 0 and bid != None and ask != None:
+            create_buy_order(bid + 1)
+            create_sell_order(ask - 1)
 
     if channel == 'lightning_executions_' + channel_symbol:
         info = param_json['message']
@@ -68,7 +113,8 @@ def on_message(ws, message):
 def on_open(ws):
     channels = [
             'lightning_executions_' + channel_symbol,
-            'lightning_ticker_' + channel_symbol,
+            'lightning_board_snapshot_' + channel_symbol,
+            'lightning_board_' + channel_symbol,
             ]
     for channel in channels:
         ws.send(json.dumps({"method": "subscribe",
