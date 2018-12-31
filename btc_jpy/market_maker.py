60010e696fc4a4414d167a9a8c59cd2d72fd32f9
Hokuto Tokutake <tokutake.hokuto@gmail.com>
2018-12-31 12:03:15 +0900

    split market maker bot per symbol

--- btc_jpy/market_maker.py
+++ btc_jpy/market_maker.py
@@ -2,7 +2,8 @@ import websocket
 import json
 import ccxt
 
-symbol = 'FX_BTC_JPY'
+channel_symbol = 'BTC_JPY'
+symbol = 'BTC/JPY'
 key_json = json.load(open('key.json'))
 bitflyer = ccxt.bitflyer({
         'apiKey': key_json['bitflyer']['api_key'],
@@ -17,7 +18,7 @@ lot = 0.01
 
 def create_buy_order(price):
     o = bitflyer.create_limit_buy_order(symbol, lot, price)
-    print('create order', o)
+    print('create buy order', o)
     o['size'] = lot
     o['price'] = price
     o['side'] = 'buy'
@@ -25,7 +26,7 @@ def create_buy_order(price):
 
 def create_sell_order(price):
     o = bitflyer.create_limit_sell_order(symbol, lot, price)
-    print('create order', o)
+    print('create sell order', o)
     o['size'] = lot
     o['price'] = price
     o['side'] = 'sell'
@@ -35,36 +36,39 @@ def on_message(ws, message):
     message_json = json.loads(message)
     param_json = message_json['params']
     channel = param_json['channel']
-    if channel == 'lightning_board_snapshot_FX_BTC_JPY':
-        print(param_json)
-
-    if channel == 'lightning_board_FX_BTC_JPY':
-        print(param_json)
-
-    if channel == 'lightning_executions_BTC_JPY':
+    if channel == 'lightning_ticker_' + channel_symbol:
         info = param_json['message']
-        print(info)
+        best_bid = info['best_bid']
+        best_ask = info['best_ask']
+        if len(open_orders) == 0:
+            create_buy_order(best_bid + 1)
+            create_sell_order(best_ask - 1)
+
+    if channel == 'lightning_executions_' + channel_symbol:
+        info = param_json['message']
         items = open_orders.items()
+        print(info)
         for id, order in items:
             for execution in info:
-                matched_order = None
                 if order['side'] == 'buy':
                     if order['id'] == execution['buy_child_order_acceptance_id']:
-                        matched_order = order
+                        order['size']  = order['size'] - execution['size']
+                        print(order)
+                        if order['size'] <= 0 :
+                            del open_orders[order['id']]
+                            print('delete order', order['id'])
                 if order['side'] == 'sell':
                     if order['id'] == execution['sell_child_order_acceptance_id']:
-                        matched_order = order
-                if matched_order:
-                    matched_order['size']  = matched_order['size'] - execution['size']
-                    if matched_order['size'] <= 0 :
-                        del open_orders[matched_order['id']]
-                        print('delete order', matched_order['id'])
+                        order['size']  = order['size'] - execution['size']
+                        print(order)
+                        if order['size'] <= 0 :
+                            del open_orders[order['id']]
+                            print('delete order', order['id'])
 
 def on_open(ws):
     channels = [
-            'lightning_executions_FX_BTC_JPY',
-            'lightning_board_snapshot_FX_BTC_JPY',
-            'lightning_board_FX_BTC_JPY',
+            'lightning_executions_' + channel_symbol,
+            'lightning_ticker_' + channel_symbol,
             ]
     for channel in channels:
         ws.send(json.dumps({"method": "subscribe",
