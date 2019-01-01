d20649f8f64cf1a72c53c9edfdde65261bd3970d
Hokuto Tokutake <tokutake.hokuto@gmail.com>
2019-01-01 16:14:04 +0900

    Use single thread to proccess message

--- market_maker.py
+++ market_maker.py
@@ -1,9 +1,20 @@
 import websocket
 import json
 import ccxt
+import threading
+import time
+import queue
+import arrow
+import datetime
 
-channel_symbol = 'FX_BTC_JPY'
-symbol = 'FX_BTC_JPY'
+is_fx = True
+
+channel_symbol = 'BTC_JPY'
+symbol = 'BTC/JPY'
+if is_fx:
+    channel_symbol = 'FX_BTC_JPY'
+    symbol = 'FX_BTC_JPY'
+
 key_json = json.load(open('key.json'))
 bitflyer = ccxt.bitflyer({
         'apiKey': key_json['bitflyer']['api_key'],
@@ -13,12 +24,15 @@ bitflyer = ccxt.bitflyer({
 bitflyer.load_markets()
 
 open_orders = {}
+fetched_open_orders = []
 
-lot = 0.01
+lot = 0.02
+
+lock = threading.Lock()
 
 def create_buy_order(price):
     o = bitflyer.create_limit_buy_order(symbol, lot, price)
-    print('create buy order', o)
+    print('create buy order', o['id'])
     o['size'] = lot
     o['price'] = price
     o['side'] = 'buy'
@@ -26,7 +40,7 @@ def create_buy_order(price):
 
 def create_sell_order(price):
     o = bitflyer.create_limit_sell_order(symbol, lot, price)
-    print('create sell order', o)
+    print('create sell order', o['id'])
     o['size'] = lot
     o['price'] = price
     o['side'] = 'sell'
@@ -35,6 +49,11 @@ def create_sell_order(price):
 bids = {}
 asks = {}
 mid_price = None
+
+lock = threading.Lock()
+open_order_count = 0
+q = queue.Queue()
+max_open_order = 6
 
 def update_board(message_json):
     mid_price = message_json['mid_price']
@@ -73,42 +92,7 @@ def print_board():
 
 
 def on_message(ws, message):
-    param_json = json.loads(message)['params']
-    channel = param_json['channel']
-    message_json = param_json['message']
-    if channel == 'lightning_board_snapshot_' + channel_symbol:
-        bids = {}
-        asks = {}
-        update_board(message_json)
-
-    if channel == 'lightning_board_' + channel_symbol:
-        update_board(message_json)
-        ask = best_ask()
-        bid = best_bid()
-        if len(open_orders) == 0 and bid != None and ask != None and ask - 1 > bid + 1 and ask - bid < 50:
-            create_buy_order(bid + 1)
-            create_sell_order(ask - 1)
-
-    if channel == 'lightning_executions_' + channel_symbol:
-        info = param_json['message']
-        items = open_orders.items()
-        for id, order in items:
-            for execution in info:
-                matched_order = None
-                matched_execution = None
-                if order['side'] == 'buy':
-                    if order['id'] == execution['buy_child_order_acceptance_id']:
-                        matched_order = order
-                        matched_execution = execution
-                if order['side'] == 'sell':
-                    if order['id'] == execution['sell_child_order_acceptance_id']:
-                        matched_order = order
-                        matched_execution = execution
-                order['size']  = order['size'] - execution['size']
-                print(order)
-                if order['size'] <= 0 :
-                    del open_orders[order['id']]
-                    print('delete order', order['id'])
+    q.put(message)
 
 def on_open(ws):
     channels = [
@@ -123,8 +107,75 @@ def on_open(ws):
 def on_close(ws):
     print('## close ##')
 
+class Bot(threading.Thread):
+    def run(self):
+        while True:
+            if q.empty():
+                continue
+
+#            import pdb; pdb.set_trace()
+            message = q.get()
+            param_json = json.loads(message)['params']
+            channel = param_json['channel']
+            message_json = param_json['message']
+            if channel == 'lightning_board_snapshot_' + channel_symbol:
+                bids = {}
+                asks = {}
+                update_board(message_json)
+                fetched_open_orders = bitflyer.fetch_open_orders(symbol)
+                expired_order = []
+                for order in fetched_open_orders:
+                    info = order['info']
+                    order_date = arrow.get(info['child_order_date'])
+                    now = datetime.datetime.now()
+                    if now.timestamp() - order_date.timestamp > 60 * 2:
+                        expired_order.append(order)
+                for order in expired_order:
+                    bitflyer.cancel_order(order['id'], symbol)
+                    if order['info']['side'] == 'buy':
+                        create_buy_order(best_bid() + 1)
+                    if order['info']['side'] == 'buy':
+                        create_buy_order(best_ask() - 1)
+
+            if channel == 'lightning_board_' + channel_symbol:
+                update_board(message_json)
+                ask = best_ask()
+                bid = best_bid()
+                if bid == None or ask == None or ask - 1 <= bid + 1:
+                    continue
+                # import pdb; pdb.set_trace()
+                if len(open_orders) < max_open_order and ask - bid < 10:
+                    create_buy_order(bid + 1)
+                    create_sell_order(ask - 1)
+                    print('len(open_orders)', len(open_orders))
+
+            if channel == 'lightning_executions_' + channel_symbol:
+                info = param_json['message']
+                items = open_orders.items()
+                closed_orders = []
+                for id, order in items:
+                    for execution in info:
+                        matched = False
+                        if order['side'] == 'buy':
+                            if order['id'] == execution['buy_child_order_acceptance_id']:
+                                matched = True
+                        if order['side'] == 'sell':
+                            if order['id'] == execution['sell_child_order_acceptance_id']:
+                                matched = True
+                        if matched:
+                            order['size']  = order['size'] - execution['size']
+                            if order['size'] <= 0 :
+                                closed_orders.append(open_orders[order['id']])
+                for order in closed_orders:
+                    print('delete order', order['id'])
+                    print(order)
+                    del open_orders[order['id']]
+
+
 if __name__ == "__main__":
     # note: reconnection handling needed.
     ws = websocket.WebSocketApp("wss://ws.lightstream.bitflyer.com/json-rpc",
                                 on_message=on_message, on_open=on_open)
+    bot = Bot()
+    bot.start()
     ws.run_forever()
