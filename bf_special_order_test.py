9dd7213357a1c05af34a318932ea886bec37c94b
Hokuto Tokutake <tokutake.hokuto@gmail.com>
2019-01-08 16:16:44 +0900

    print winning rate and pl

--- bf_special_order_test.py
+++ bf_special_order_test.py
@@ -7,6 +7,9 @@ import numpy as np
 symbol = 'FX_BTC_JPY'
 
 lot = 0.01
+target_spread = 1 / lot / 2
+print('lot:', lot)
+print('target_spread:', target_spread)
 
 def now():
     return datetime.datetime.now()
@@ -22,11 +25,14 @@ bf = ccxt.bitflyer({
 
 bf.load_markets()
 
+def getcollateral():
+    return bf.request('getcollateral', 'private')['collateral']
+
 def get_target_price():
     orderBook = bf.fetchOrderBook(symbol)
     bids = orderBook['bids']
     bids.sort(reverse = True)
-    depth = 1
+    depth = 0.1
     target_price = None
     total_size = 0
     for bid in bids:
@@ -43,7 +49,7 @@ print(datetime.datetime.now())
 def send_ifdoco(target_price):
     return bf.request("sendparentorder", "private", "POST", {
       "order_method": "IFDOCO",
-      "minute_to_expire": 1,
+      "minute_to_expire": 5,
       "time_in_force": "GTC",
       "parameters": [{
         "product_code": symbol,
@@ -56,47 +62,73 @@ def send_ifdoco(target_price):
         "product_code": symbol,
         "condition_type": "LIMIT",
         "side": "SELL",
-        "price": target_price + 51,
+        "price": target_price + target_spread + 1,
         "size": lot
       },
       {
         "product_code": symbol,
         "condition_type": "STOP_LIMIT",
         "side": "SELL",
-        "price": target_price - 50,
-        "trigger_price": target_price - 50,
+        "price": target_price - target_spread,
+        "trigger_price": target_price - round(1.0 * target_spread),
         "size": lot
       }]
     })
 
-def get_order_status(order):
+def get_parent_order(order):
     orders = bf.request('getparentorders', 'private', params = {'product_code': 'FX_BTC_JPY'})
     for order in orders:
         if request_order['parent_order_acceptance_id'] == order['parent_order_acceptance_id']:
-            return order['parent_order_state']
+            return order
     return None
 
-for i in range(10):
+def get_child_order(parent_order_id):
+    return bf.request('getchildorders', 'private', params = {'product_code': 'FX_BTC_JPY', 'parent_order_id': parent_order_id})
+
+win = 0
+lose = 0
+draw = 0
+pl = 0
+for i in range(20):
     print(datetime.datetime.now())
-    request_order = send_ifdoco(get_target_price())
+    target_price = get_target_price()
+    request_order = send_ifdoco(target_price)
     print(request_order)
     request_sent_time = now()
 
     is_active = False
-    prev_status = None
+    prev_state = None
+    state = None
+    parent_order = None
     while True:
-        status = get_order_status(request_order)
-        if prev_status != status:
-            print(status)
-        if status == 'COMPLETED' or status == 'CANCELED' or status == 'EXPIRED':
+        parent_order = get_parent_order(request_order)
+        print(parent_order)
+        if parent_order:
+            state = parent_order['parent_order_state']
+        if prev_state != state:
+            print(state)
+        if state == 'COMPLETED' or state == 'CANCELED' or state == 'EXPIRED':
+#            import pdb; pdb.set_trace()
             break
-        if prev_status != 'ACTIVE' and status == 'ACTIVE':
+        if prev_state != 'ACTIVE' and state == 'ACTIVE':
             print('time untile active:', elapsed_time(request_sent_time))
-        if status == 'ACTIVE':
+        if state == 'ACTIVE':
             is_active = True
         if is_active:
             time.sleep(2)
-        prev_status = status 
+        prev_state = state 
 
-    print(bf.request('getcollateral', 'private'))
+    child_order = bf.request('getchildorders', 'private', params = {'product_code': 'FX_BTC_JPY', 'count': '1'})[0]
+    if state != 'COMPLETED' or target_price == child_order['price']:
+        draw += 1
+    elif target_price < child_order['price']:
+        win += 1
+        pl += child_order['price'] - target_price
+    else:
+        lose += 1
+        pl += child_order['price'] - target_price
+
+    print('collateral:', getcollateral());
+    print('win:{}, lose:{}, draw:{}, pl:{}'.format(win, lose, draw, pl))
     # import pdb; pdb.set_trace()
+
