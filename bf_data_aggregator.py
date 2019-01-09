e202e0fe286a7238429be4d6432e3385958889a5
Hokuto Tokutake <tokutake.hokuto@gmail.com>
2019-01-10 00:59:43 +0900

    Remove print. add sleep to reduce cpu usage

--- bf_data_aggregator.py
+++ bf_data_aggregator.py
@@ -2,6 +2,7 @@ import datetime
 from pymongo import MongoClient
 import pprint
 import bitflyer_websocket
+import time
 
 client = MongoClient()
 db = client.test_database
@@ -10,18 +11,13 @@ bf = bitflyer_websocket.BitflyerWebsocket()
 while True:
     while not bf.execution_queue.empty():
         message = bf.execution_queue.get()
-        print(message)
-        inserted_id = db.board_executions.insert_one(message).inserted_id
-        print(inserted_id)
+        db.executions.insert_one(message)
 
     while not bf.board_snapshot_queue.empty():
         message = bf.board_snapshot_queue.get()
-        print(message)
-        inserted_id = db.board_snapshots.insert_one(message).inserted_id
-        print(inserted_id)
+        db.board_snapshots.insert_one(message)
 
     while not bf.board_update_queue.empty():
         message = bf.board_update_queue.get()
-        print(message)
-        inserted_id = db.board_updates.insert_one(message).inserted_id
-        print(inserted_id)
+        db.board_updates.insert_one(message)
+    time.sleep(0.1)
